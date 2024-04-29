09b0a9c3a3f8e5a79e55ddfeaf3db943ca75aa4c
João Fonseca <joao.fonseca@ua.pt>
2024-04-29 20:29:47 +0100

    update benchmarks

--- prog2/benchmark.sh
+++ prog2/benchmark.sh
@@ -1,27 +1,32 @@
 # Usage: ./benchmark.sh
 # Description: Compiles the source code, runs the mpi-based bitonic sort program, and outputs the results in a
-#              "results" file, for each configuration of array sizes (32, 256K, 1M, 16M) and processes (1, 2, 4, 8).
+#              "results" folders, for each configuration of array sizes (32, 256K, 1M, 16M) and processes (1, 2, 4, 8).
 # Example: ./benchmark.sh
 
-OUTPUT_FILE="results.txt"
-FOLDER_NUMBERS="data"
-FILE_NUMBERS="datSeq32.bin datSeq256K.bin datSeq1M.bin datSeq16M.bin"
+OUTPUT_FOLDER="results"
+NUMBERS_FOLDER="data"
+NUMBERS_SIZES="32 256K 1M 16M"
 N_PROCS="1 2 4 8"
+N_ITERATIONS=15
 
-# Create the output file
-rm -f $OUTPUT_FILE
-touch $OUTPUT_FILE
+# Create the output folder
+mkdir -p $OUTPUT_FOLDER
 
 # Compile the source code
-mpicc -O3 -o bmprog2 mpiBitonic.c
+mpicc -Wall -O3 -o bmprog2 mpiBitonic.c sortUtils.c
 
 # Run the program for each configuration of processes and array sizes
-for file in $FILE_NUMBERS; do
+for size in $NUMBERS_SIZES; do
   for procs in $N_PROCS; do
-    echo "Running program with file $file and $procs processes..."
-    echo "File: $file; Processes: $procs" >> $OUTPUT_FILE
-    mpiexec -n $procs ./bmprog2 -f $FOLDER_NUMBERS/$file | grep '^\[TIME\]' >> $OUTPUT_FILE
-    echo "" >> $OUTPUT_FILE
+    echo "Running program $N_ITERATIONS times for $size numbers and $procs processes..."
+    # Create the output file
+    OUTPUT_FILE="$OUTPUT_FOLDER/n$size-p$procs.txt"
+    rm -f $OUTPUT_FILE
+    touch $OUTPUT_FILE
+    # Run the program and save the results
+    for i in $(seq 1 $N_ITERATIONS); do
+      mpiexec -n $procs ./bmprog2 -f $NUMBERS_FOLDER/datSeq$size.bin | grep -Po 'Time elapsed\s*:\s*\K[0-9.]+' >> $OUTPUT_FILE
+    done
   done
 done
 
